79c3ef3bc9706b749de3658f7d3dfc7456dc8fd3
lineshogan <lineshogan@foxmail.com>
2025-05-08 21:28:41 +0800

    del secret

--- scripts/set/set_vars.sh
+++ scripts/set/set_vars.sh
@@ -48,7 +48,7 @@ export CACHE_DIR="${PROJECT_DIR}/.cache"
 export WANDB_CACHE_DIR="${CACHE_DIR}"
 export TRITON_CACHE_DIR="${CACHE_DIR}/triton_cache"
 
-export HF_TOKEN=""
+export HF_TOKEN="no token"
 git config --global credential.helper store
 huggingface-cli login --token $HF_TOKEN --add-to-git-credential
 
